e31f6aed6f4e60c8bbfec582e9c6fcdda26ca3bd
Amey <ameyk1@umbc.edu>
2020-03-07 14:49:03 -0800

    smallerThanCurrent:P1365:leetcode
    https://leetcode.com/contest/weekly-contest-178/problems/how-many-numbers-are-smaller-than-the-current-number/

---
+++ smaller-than-current.py
@@ -0,0 +1,28 @@
+import numpy as np
+class Solution_wSort():
+    def smallerNumbersThanCurrent(self, nums):
+        output = []
+        nums_sorted = tuple(np.sort(nums))
+        for i in nums:
+            output.append(nums_sorted.index(i))
+        return output
+
+class Solution():
+    def smallerNumbersThanCurrent(self, nums):
+        nums_bucket = [0]*101
+        for num in nums:
+            nums_bucket[num]+=1
+        accumulate = [0]*101
+        for i in range(1,101):
+            accumulate[i]= accumulate[i-1]+nums_bucket[i-1]
+        return [accumulate[num] for num in nums]
+def main():
+    nums = [8, 8, 77, 1, 2]
+    print(nums)
+    sol = Solution_wSort()
+    print(sol.smallerNumbersThanCurrent(nums))
+    sol = Solution()
+    print(sol.smallerNumbersThanCurrent(nums))
+    
+if __name__ == '__main__':
+    main()
